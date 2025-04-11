6bc754421d689c93ceb2c8479683b84b1ec67904
Zulpukarov Marsel <zulpukarovmarsel17@gmail.com>
2025-04-12 01:13:46 +0600

    fixed bug

---
+++ src/config/settings/production.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from decouple import config
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
